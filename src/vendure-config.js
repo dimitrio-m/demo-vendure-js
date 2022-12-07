b5e82c84819b11d75ed9b10615d94239fbbca006
Dimitrio Mandamadiotis <me@dimitriosmandamadiotis.com>
2022-12-07 09:39:14 -0400

    Env PORT

--- src/vendure-config.js
+++ src/vendure-config.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 const config = {
     apiOptions: {
-        port: 3000,
+        port: process.env.PORT || 3000,
         adminApiPath: 'admin-api',
         adminApiPlayground: {
             settings: {
